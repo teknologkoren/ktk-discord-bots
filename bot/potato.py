7ffee023ed5eae5ef5060040493ef0b53a915d6e
Webmaster Kongl. Teknologkören <webmaster@teknologkoren.se>
2025-06-06 10:27:21 +0200

    Update potato bot Youtube links.

--- bot/potato.py
+++ bot/potato.py
@@ -11,7 +11,23 @@ videos = {
     5: ("https://www.youtube.com/watch?v=DTXO81iC-IY", "Över eld i vatten"),
     6: ("https://www.youtube.com/watch?v=qQrrYExkD9c", "Duka bord med kokt potatis"),
     7: ("https://www.youtube.com/watch?v=NRpALWQpB0g", "Knölman"),
-    # TODO: add the rest of them
+    8: ("https://www.youtube.com/watch?v=gM8QRADdt0I", "O helga knöl"),
+    9: ("https://www.youtube.com/watch?v=zeiy8swD2uA", "Veni Potatisen"),
+    10: ("https://www.youtube.com/watch?v=qIg0S5l95-s", "Kommer icke Knölof Tryggvasson"),
+    11: ("https://www.youtube.com/watch?v=iB0WVRsoka4", "Staffan var en potatisdräng"),
+    12: ("https://www.youtube.com/watch?v=zRNf02-WDOI", "God potatis i detta skal"),
+    13: ("https://www.youtube.com/watch?v=QbcdeCrNIqo", "Strålande jordeknöl"),
+    14: ("https://www.youtube.com/watch?v=FqvhXl_by_Y", "Sankta Potatis"),
+    15: ("https://www.youtube.com/watch?v=LHABeMFSDKg", "Fååunnelig ädh khåågge"),
+    16: ("https://www.youtube.com/watch?v=uW0rnR69IDM", "God rest ye merry Potato"),
+    17: ("https://www.youtube.com/watch?v=lQ-Ci8y__dQ", "Det strålar en rotfrukt"),
+    18: ("https://www.youtube.com/watch?v=cZD-HOEMYhU", "Sådan är Potatisplantan"),
+    19: ("https://www.youtube.com/watch?v=-4zk195PcSU", "Potatisen"),
+    20: ("https://www.youtube.com/watch?v=Fb8QtmNmOXo", "Det är en rot utsprungen"),
+    21: ("https://www.youtube.com/watch?v=2PapRwhMy9k", "Knöl, Potatis, Tenorvän"),
+    22: ("https://www.youtube.com/watch?v=3eOWZargoY0", "Gul Gul"),
+    23: ("https://www.youtube.com/watch?v=CxTJJTNfz5s", "Nu kokas tusen juleknöl"),
+    24: ("https://www.youtube.com/watch?v=3Ek40aWEulo", "Knölevangeliet"),
 }
 
 async def post(bot):
